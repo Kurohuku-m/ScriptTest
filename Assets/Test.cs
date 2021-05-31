149b185b7da036bd6c74cf987abc635ddce2c6f1
Kurohuku-m <84332032+Kurohuku-m@users.noreply.github.com>
2021-05-31 19:19:56 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] points = { 1, 2, 3, 5, 7 };
+
+        // 配列の各要素の値を順番に表示する
+        for (int i = 0; i < 5; i++)
+            {
+            Debug.Log(points[i]);
+            }
+
+        // 配列の各要素の値を順番に表示する
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(points[i]);
+        }
+    } 
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
---
+++ Assets/Test2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss
+{
+    private int mp = 53;   //　マジックポイント
+
+    // 魔法攻撃用のMagic関数
+    public void Magic(int consumption)
+    {
+        if (this.mp >= 5)
+        {
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+            // mpが5以上の場合は残りmpを減らす
+            this.mp -= consumption;
+        }
+        else
+        {
+            // mpが5より小さい場合
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
+    }
+
+}
+
+public class Test2 : MonoBehaviour
+{
+
+    void Start2()
+    {
+        //10回処理を繰り返す
+        for (int consumption = 0; consumption < 10; consumption++)
+        {
+            // Bossクラスの変数を宣言してインスタンスを代入
+            Boss lastboss = new Boss();
+
+            // Magic関数を呼び出す
+            lastboss.Magic(5);
+        }
+    }
+
+    // Update is called once per frame
+    void Update2()
+    {
+
+
+    }
+}
